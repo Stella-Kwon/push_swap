94da9e29c46e6913fb7cde04e253eacd508530b9
skwon2 <skwon2@c2r6p10.hive.fi>
2024-02-10 18:36:02 +0200

    fixtheprintf

--- default.c
+++ default.c
@@ -6,10 +6,28 @@
 /*   By: skwon2 <skwon2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 15:22:41 by skwon2            #+#    #+#             */
-/*   Updated: 2024/02/10 15:34:20 by skwon2           ###   ########.fr       */
+/*   Updated: 2024/02/10 18:28:27 by skwon2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-rs {}
+void error_check(int arg, char **args)
+{
+    int i;
+    i =  0;
+    
+    // if (args == NULL)
+    //     return;
+    while (i < arg)
+    {
+        if (sizeof(args[i]) > sizeof(int) | sizeof(args[i]) != sizeof(int))
+            write(1, "ERROR", 6);
+    }
+}
+
+int main(int arg, char **args)
+{
+    error_check(arg,args);
+    return 0;
+}
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: skwon2 <skwon2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 15:24:49 by skwon2            #+#    #+#             */
-/*   Updated: 2024/02/10 15:38:42 by skwon2           ###   ########.fr       */
+/*   Updated: 2024/02/10 18:32:17 by skwon2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,6 @@
 # define PUSH_SWAP_H
 
 # include "./ft_printf/ft_printf.h"
+# include "./ft_printf/libft/libft.h"
 
 #endif
