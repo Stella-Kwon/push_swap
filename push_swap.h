81d158d0cb4be4ac76e1a76d22d5c8bd6af7c002
skwon2 <skwon2@c1r5p12.hive.fi>
2024-02-13 15:50:45 +0200

    make error.check

--- push_swap.h
+++ push_swap.h
@@ -6,14 +6,17 @@
 /*   By: skwon2 <skwon2@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 15:24:49 by skwon2            #+#    #+#             */
-/*   Updated: 2024/02/10 18:32:17 by skwon2           ###   ########.fr       */
+/*   Updated: 2024/02/13 14:38:15 by skwon2           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-# include "./ft_printf/ft_printf.h"
-# include "./ft_printf/libft/libft.h"
-
+// # include "./ft_printf/ft_printf.h"
+// # include "./ft_printf/libft/libft.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <limits.h>
 #endif
